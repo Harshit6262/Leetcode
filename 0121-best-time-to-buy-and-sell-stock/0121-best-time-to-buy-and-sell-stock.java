e765bb5dcfa1386b1f1b13fdc11e6507528cb300
Harshit Jaiswal <harshitjaiswal0705@gmail.com>
2026-08-26 09:56:52 +0530

    08-26-2026 0121-best-time-to-buy-and-sell-stock Easy 2 ms (44%) 94.3 MB (71.13%)

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -1,17 +1,16 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int minPrice = prices[0];
-        int maxProfit = 0;
-        for(int i = 1; i<prices.length; i++){
-            if(prices[i] < minPrice){
-                minPrice = prices[i];
+        int buyPrice = Integer.MAX_VALUE;
+        int maxPrice = 0;
+        for(int i = 0; i < prices.length; i++){
+            if(buyPrice < prices[i]){
+                int profit = prices[i] - buyPrice;
+                maxPrice = Math.max(profit, maxPrice);
             }
-            int profit = prices[i] - minPrice;
-            if (profit > maxProfit){
-                maxProfit = profit;
+            else {
+                buyPrice = prices[i];
             }
         }
-        return maxProfit;
-        
+        return maxPrice;
     }
 }
