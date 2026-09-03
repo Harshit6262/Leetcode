e92a4cdeace4aa755d0ba07523be474b5a17d7b0
Harshit Jaiswal <harshitjaiswal0705@gmail.com>
2026-09-03 16:03:48 +0530

    09-03-2026 0125-valid-palindrome Easy 2 ms (99.29%) 44 MB (96.06%)

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -2,14 +2,14 @@ class Solution {
     public boolean isPalindrome(String s) {
         int left = 0;
         int right = s.length() - 1;
-        while (left < right){
-            while (left < right && !Character.isLetterOrDigit(s.charAt(left))){
+        while(left < right){
+            while(left < right && !Character.isLetterOrDigit(s.charAt(left))){
                 left++;
             }
             while (left < right && !Character.isLetterOrDigit(s.charAt(right))){
                 right--;
             }
-            if (Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))) {
+            if(Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))){
                 return false;
             }
             left++;
@@ -17,4 +17,5 @@ class Solution {
         }
         return true;
     }
+    
 }
